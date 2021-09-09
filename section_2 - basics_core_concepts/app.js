63d7ea65d710e73619ee19424844dc17f97ba37d
Dimitar Jordanov <theshadowbyte@gmail.com>
2021-09-09 16:26:52 -0400

    Beginning of the basics / core concepts section (interpolation, v-binds, using data in methods using this

---
+++ section_2 - basics_core_concepts/app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp(
+    {
+        data: function() {
+            return {
+                courseGoal: 'First goal',
+                courseGoalA: 'Finish the course and learn Vue',
+                courseGoalB: 'Master Vue and build apps',
+                vueLink: 'https://vuejs.org'
+            };
+        },
+        methods: {
+            outputGoal: function() {
+                const randomNumber = Math.random();
+                if (randomNumber < 0.5) {
+                    return this.courseGoalA;
+                }
+                else {
+                    return this.courseGoalB;
+                }
+            }
+        }
+    }
+);
+app.mount("#user-goal");
